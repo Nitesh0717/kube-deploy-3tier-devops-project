80b5170fdc5c34ec2f15cedc893bb275659fe61d
Nitesh0717 <singhnitesh0204@gmail.com>
2026-04-15 22:54:48 +0530

    improved UI design for todo app

--- app/app.js
+++ app/app.js
@@ -1,88 +1,84 @@
 const express = require('express'),
-    bodyParser = require('body-parser'),
-    // In order to use PUT HTTP verb to edit item
-    methodOverride = require('method-override'),
-    // Mitigate XSS using sanitizer
-    sanitizer = require('sanitizer'),
-    app = express(),
-    port = 8000
+bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+sanitizer = require('sanitizer'),
+app = express(),
+port = 8000
+
+app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({
-    extended: false
-}));
-// https: //github.com/expressjs/method-override#custom-logic
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
-        let method = req.body._method;
-        delete req.body._method;
-        return method
-    }
+extended:false
 }));
 
+app.use(methodOverride(function (req, res) {
+if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+let method = req.body._method;
+delete req.body._method;
+return method;
+}
+}));
 
 let todolist = [];
 
-/* The to do list and the form are displayed */
-app.get('/todo', function (req, res) {
-        res.render('todo.ejs', {
-            todolist,
-            clickHandler: "func1();"
-        });
-    })
+app.get('/todo', function(req,res){
+res.render('todo.ejs',{
+todolist
+});
+})
 
-    /* Adding an item to the to do list */
-    .post('/todo/add/', function (req, res) {
-        // Escapes HTML special characters in attribute values as HTML entities
-        let newTodo = sanitizer.escape(req.body.newtodo);
-        if (req.body.newtodo != '') {
-            todolist.push(newTodo);
-        }
-        res.redirect('/todo');
-    })
+.post('/todo/add', function(req,res){
+let newTodo = sanitizer.escape(req.body.newtodo);
 
-    /* Deletes an item from the to do list */
-    .get('/todo/delete/:id', function (req, res) {
-        if (req.params.id != '') {
-            todolist.splice(req.params.id, 1);
-        }
-        res.redirect('/todo');
-    })
+if(newTodo!=''){
+todolist.push(newTodo);
+}
 
-    // Get a single todo item and render edit page
-    .get('/todo/:id', function (req, res) {
-        let todoIdx = req.params.id;
-        let todo = todolist[todoIdx];
+res.redirect('/todo');
+})
 
-        if (todo) {
-            res.render('edititem.ejs', {
-                todoIdx,
-                todo,
-                clickHandler: "func1();"
-            });
-        } else {
-            res.redirect('/todo');
-        }
-    })
+.get('/todo/delete/:id', function(req,res){
 
-    // Edit item in the todo list 
-    .put('/todo/edit/:id', function (req, res) {
-        let todoIdx = req.params.id;
-        // Escapes HTML special characters in attribute values as HTML entities
-        let editTodo = sanitizer.escape(req.body.editTodo);
-        if (todoIdx != '' && editTodo != '') {
-            todolist[todoIdx] = editTodo;
-        }
-        res.redirect('/todo');
-    })
-    /* Redirects to the to do list if the page requested is not found */
-    .use(function (req, res, next) {
-        res.redirect('/todo');
-    })
+let id = req.params.id;
 
-    .listen(port, function () {
-        // Logging to console
-        console.log(`Todolist running on http://0.0.0.0:${port}`)
-    });
-// Export app
+if(id!=''){
+todolist.splice(id,1);
+}
+
+res.redirect('/todo');
+})
+
+.get('/todo/:id', function(req,res){
+
+let id = req.params.id;
+let todo = todolist[id];
+
+res.render('edititem.ejs',{
+id,
+todo
+});
+
+})
+
+.put('/todo/edit/:id', function(req,res){
+
+let id = req.params.id;
+let editTodo = sanitizer.escape(req.body.editTodo);
+
+if(editTodo!=''){
+todolist[id] = editTodo;
+}
+
+res.redirect('/todo');
+
+})
+
+.use(function(req,res){
+res.redirect('/todo');
+})
+
+.listen(port,function(){
+console.log(`Todolist running on http://0.0.0.0:${port}`);
+});
+
 module.exports = app;
